349e9d9d867febc3c46aac3e56644816fdc9ce8d
Evan <evan.whitfield@gmail.com>
2013-08-29 19:57:56 -0700

    Update p30.py

--- p30.py
+++ p30.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+"""
+
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+    1634 = 1^4 + 6^4 + 3^4 + 4^4
+    8208 = 8^4 + 2^4 + 0^4 + 8^4
+    9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+As 1 = 1^4 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+"""
+
 def sum_fifths(n):
    total = 0
    while n > 0:
