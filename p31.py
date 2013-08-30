cb0ff7ecef5265a6b158a7d039647f36faf482fe
Evan <evan.whitfield@gmail.com>
2013-08-29 19:59:54 -0700

    Update p31.py

--- p31.py
+++ p31.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
+"""
+
+In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+
+    1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+
+It is possible to make £2 in the following way:
+
+    1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+
+How many different ways can £2 be made using any number of coins?
+
+"""
+
 #coin denominations
 den = [200,100,50,20,10,5,2,1]
 
-#cache - m[i][j] = num ways of making 
-#matrix = []
-#for i in range(10):
-#   matrix.append([0,0,0,0,0,0,0,0])
-
 cache = {}
+
 #returns the number of ways of making n pence
 # using den >= index i
 def ways(n, i):
