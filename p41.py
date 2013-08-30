888d43403be216366ff4adb9a9587ff450192de6
Evan <evan.whitfield@gmail.com>
2013-08-29 20:11:09 -0700

    Update p41.py

--- p41.py
+++ p41.py
@@ -2,9 +2,12 @@
 import math
 
 """
-We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 
+2143 is a 4-digit pandigital and is also prime.
 
 What is the largest n-digit pandigital prime that exists?
+
 """
 
 # Note: pandigitals with 8 or 9 digits are not prime
