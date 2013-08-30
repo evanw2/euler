83606b335467f647c194f07bf848e4bb1086a142
Evan <evan.whitfield@gmail.com>
2013-08-29 19:25:14 -0700

    Update p17.py

--- p17.py
+++ p17.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+"""
+
+
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19
+letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one
+hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+"""
 
 def speak(n):
    ones = {1: "one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six",
@@ -41,7 +53,7 @@ def letters(s):
 
 sum = 0
 for i in range(1,1001):
-   print speak(i), letters(speak(i))
+   #print speak(i), letters(speak(i))
    sum += letters( speak(i) )
 
 print sum
