83373ffb262ff480c8b5ccea85cc677c0919d16a
Evan <evan.whitfield@gmail.com>
2013-08-29 19:52:58 -0700

    Update p27.py

--- p27.py
+++ p27.py
@@ -4,7 +4,7 @@
 
 Euler discovered the remarkable quadratic formula:
 
-n² + n + 41
+n^2 + n + 41
 
 It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. However, when n = 40, 
 40^2 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41^2 + 41 + 41 is clearly divisible
