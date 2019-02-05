4693c05907d7b6dd6b17bc4b2a0c6816fb345311
Evan Whitfield <evan.whitfield@gmail.com>
2019-02-04 21:21:48 -0800

    Formatting

--- p023/p023.py
+++ p023/p023.py
@@ -19,32 +19,31 @@ Find the sum of all the positive integers which cannot be written as the sum of 
 """
 
 def abundant(n):
-   total = 0
-   for i in range(1,n):
-      if n %i == 0:
-         total += i
-   return total > n
+  total = 0
+  for i in range(1, n):
+    if n % i == 0:
+      total += i
+  return total > n
 
 nums = []
 
-for n in range(1,28123):
-   if abundant(n):
-      nums.append(n)
+for n in range(1, 28123):
+  if abundant(n):
+    nums.append(n)
 sum2 = {}
-for n in range(1,28123):
-   sum2[n] = False
+for n in range(1, 28123):
+  sum2[n] = False
 
 for num1 in nums:
-   for num2 in nums:
-      if num1 + num2 < 28123:
-         sum2[num1 + num2] = True
+  for num2 in nums:
+    if num1 + num2 < 28123:
+      sum2[num1 + num2] = True
          
 total = 0
 for n in range(1, 28123):
-   if not sum2[n]:
-      total += n
+  if not sum2[n]:
+    total += n
 
-print total
+print(total)
       
-
 
